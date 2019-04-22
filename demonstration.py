8fa3d11a9d76e00b0cfaa378a4985ad61ba3f1cc
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2019-04-22 14:13:26 -0700

    Initial import.

---
+++ demonstration.py
@@ -0,0 +1,12 @@
+
+###########################################################################
+#   Demonstration for Illumina candidacy. Emily Williams, 22 April 2019   #
+###########################################################################
+
+#
+# import system libraries
+#
+import pprint as pp
+import numpy as np
+import pandas as pd
+
