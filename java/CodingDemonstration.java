f5141d74e82bfc06dd88c7a980bc7ecfadc4b773
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2019-04-22 14:30:32 -0700

    Adding a template in case Java is requested.

---
+++ java/CodingDemonstration.java
@@ -0,0 +1,10 @@
+
+/*
+  Live coding demonstration for Illumina candidacy. Emily Williams, 22 April 2019   #
+*/
+
+class CodingDemonstration {
+    public static void main(String[] args) {
+        System.out.println("Coding demonstration template."); // Display the string.
+    }
+}
