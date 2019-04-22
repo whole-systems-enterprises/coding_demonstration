f6e5a124d9c96bfa159a9b880dd5695f2fde0167
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2019-04-22 14:33:23 -0700

    Added a comment.

--- java/CodingDemonstration.java
+++ java/CodingDemonstration.java
@@ -1,10 +1,13 @@
 
 /*
-  Live coding demonstration for Illumina candidacy. Emily Williams, 22 April 2019   #
+  Live coding demonstration for Illumina candidacy. Emily Williams, 22 April 2019
 */
 
+/*
+  Class containing the main method:
+*/
 class CodingDemonstration {
     public static void main(String[] args) {
-        System.out.println("Coding demonstration template."); // Display the string.
+        System.out.println("Coding demonstration template.");
     }
 }
