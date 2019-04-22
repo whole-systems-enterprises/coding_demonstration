deb5011597ebf6d18baffa7c1c6c4dc7c2b3ab09
whole-systems-enterprises <whole.systems.enterprises@gmail.com>
2019-04-22 14:23:41 -0700

    Added useful libraries.

--- demonstration.py
+++ demonstration.py
@@ -1,12 +1,17 @@
 
-###########################################################################
-#   Demonstration for Illumina candidacy. Emily Williams, 22 April 2019   #
-###########################################################################
+#######################################################################################
+#   Live coding demonstration for Illumina candidacy. Emily Williams, 22 April 2019   #
+#######################################################################################
 
 #
 # import system libraries
 #
+# I don't know what I need yet, so just setting up my standard kit:
+#
 import pprint as pp
 import numpy as np
 import pandas as pd
+
+
+
 
